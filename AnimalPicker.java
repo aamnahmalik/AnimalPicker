7266e6f60949ebd1c48ef074e70d9486ce065c52
aamnahmalik <aamnah@icloud.com>
2023-02-23 00:38:33 -0500

    gives user choice between dog and cat, then prints placeholder text

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -1,9 +1,23 @@
 package AnimalPicker;
 
+import java.util.Scanner;
+
 public class AnimalPicker 
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Would you like to see a dog or cat?");
+        String animalChoice = input.next();
+
+        if(animalChoice.equalsIgnoreCase("dog"))
+        {
+            System.out.println("DOG ASCII ART HERE");
+        }
+        else
+        {
+            System.out.println("CAT ASCII ART HERE");
+        }
     }
 }
